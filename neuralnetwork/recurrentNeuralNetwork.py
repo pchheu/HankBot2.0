a4b7ce9a3d747b7df0964e30249e50f37459ed8f
pchheu <pchheu@gmail.com>
2018-04-07 17:15:36 -0400

    Init recurrent neural network file

---
+++ neuralnetwork/recurrentNeuralNetwork.py
@@ -0,0 +1,13 @@
+# Import necessary libraries
+from __future__ import print_function
+from keras.callbacks import LambdaCallback
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+from keras.utils.data_utils import get_file
+import numpy as np
+import random
+import sys
+import io
+
